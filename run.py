1968a9d06341e5029b565663cf7521bedc94486c
Yuriy Guts <yuriy.guts@gmail.com>
2023-06-11 16:16:28 +0300

    CLI improvements, Ctrl-C support

--- run.py
+++ run.py
@@ -38,40 +38,48 @@ def run_agent(agent: Agent, env: gym.Env, verbose: bool = False) -> EpisodeHisto
         episode_history_plotter.create_plot()
 
     # Main simulation/learning loop.
-    for episode_index in range(max_episodes_to_run):
-        observation, _ = env.reset()
-        action = agent.begin_episode(observation)
+    print("Running the environment. To stop, press Ctrl-C.")
+    try:
+        for episode_index in range(max_episodes_to_run):
+            observation, _ = env.reset()
+            action = agent.begin_episode(observation)
 
-        for timestep_index in range(max_timesteps_per_episode):
-            # Perform the action and observe the new state.
-            observation, reward, terminated, _, _ = env.step(action)
+            for timestep_index in range(max_timesteps_per_episode):
+                # Perform the action and observe the new state.
+                observation, reward, terminated, _, _ = env.step(action)
 
-            # Log the current state.
-            if verbose:
-                log_timestep(timestep_index, action, reward, observation)
-
-            # If the episode has ended prematurely, penalize the agent.
-            if terminated and timestep_index < max_timesteps_per_episode - 1:
-                reward = -max_episodes_to_run
-
-            # Get the next action from the learner, given our new state.
-            action = agent.act(observation, reward)
-
-            # Record this episode to the history and check if the goal has been reached.
-            if terminated or timestep_index == max_timesteps_per_episode - 1:
-                print(f"Episode {episode_index + 1} finished after {timestep_index + 1} timesteps.")
-
-                episode_history[episode_index] = timestep_index + 1
+                # Log the current state.
                 if verbose:
-                    episode_history_plotter.update_plot(episode_index)
+                    log_timestep(timestep_index, action, reward, observation)
 
-                if episode_history.is_goal_reached(episode_index):
-                    print(f"SUCCESS: Goal reached after {episode_index + 1} episodes!")
-                    return episode_history
+                # If the episode has ended prematurely, penalize the agent.
+                if terminated and timestep_index < max_timesteps_per_episode - 1:
+                    reward = -max_episodes_to_run
 
-                break
+                # Get the next action from the learner, given our new state.
+                action = agent.act(observation, reward)
 
-    print(f"FAILURE: Goal not reached after {max_episodes_to_run} episodes.")
+                # Record this episode to the history and check if the goal has been reached.
+                if terminated or timestep_index == max_timesteps_per_episode - 1:
+                    print(
+                        f"Episode {episode_index + 1} "
+                        f"finished after {timestep_index + 1} timesteps."
+                    )
+
+                    episode_history[episode_index] = timestep_index + 1
+                    if verbose:
+                        episode_history_plotter.update_plot(episode_index)
+
+                    if episode_history.is_goal_reached(episode_index):
+                        print(f"SUCCESS: Goal reached after {episode_index + 1} episodes!")
+                        return episode_history
+
+                    break
+
+        print(f"FAILURE: Goal not reached after {max_episodes_to_run} episodes.")
+    except KeyboardInterrupt:
+        print("WARNING: Terminated by user request.")
+
     return episode_history
 
 
@@ -115,6 +123,7 @@ def save_history(history: EpisodeHistory, experiment_dir: str) -> pathlib.Path:
     file_path = experiment_dir_path / "episode_history.csv"
     dataframe = pd.DataFrame(history.episode_lengths, columns=["length"])
     dataframe.to_csv(file_path, header=True, index_label="episode")
+    print(f"Episode history saved to {file_path}")
     return file_path
 
 
